11b95e75fa23bbee71ab37ef3ba16f42dfb4661e
FernandoNascimento82 <136522189+FernandoNascimento82@users.noreply.github.com>
2024-01-27 21:49:49 -0300

    Update stremlit_petroleo.py

--- stremlit_petroleo.py
+++ stremlit_petroleo.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import itertools
 from statsmodels.tsa.arima.model import ARIMA
 # Importe outras bibliotecas necessárias
 
